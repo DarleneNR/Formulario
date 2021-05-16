99949e0e8cd6edd1ee96c8002ac043fcb98b26a4
Darlene Navarro <dar.navarror@duocuc.cl>
2021-05-16 02:01:57 -0400

    Ajustes

--- connection.php
+++ connection.php
@@ -7,13 +7,9 @@ define('DATABASE', 'ejercicio_formulario1');
 
 // Aqui se realiza la conexion de base de datos
 try {
-
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
-
 } catch (PDOException $e) {
-
     exit("Error: " . $e->getMessage());
-
 }
 
 ?>
